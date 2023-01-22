0cebf67318367f19ebb99de95c9b53a33b9e071e
xi2593-rgupta <rishabh.gupta1@xebia.com>
2023-01-22 17:47:46 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# bookings
+# Bookings and Reservations
+
+This is the repository for my bookings and reservations project.
+
+- Build in Go version 1.19
+- Uses the chi router
+- Uses alex edwards SCS session management
+- Uses no surf
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/rish1508/bookings/pkg/config"
+	"github.com/rish1508/bookings/pkg/models"
+	"github.com/rish1508/bookings/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets th repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	reportIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", reportIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "hello again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
